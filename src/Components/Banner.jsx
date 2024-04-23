ad2c498ce7250ccf3ae5e8bcf6de1fe0a7898212
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-04-23 13:43:58 +0600

    navbar done. Now building banner

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div className="bg-[url('https://i.imgur.com/0n6Fu6u.jpg)] h-[800px]">
+            
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,9 +1,12 @@
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is Navbar section</h1>
-        </div>
+        <nav className="bg-[#372727] bg-[url('https://i.imgur.com/4KthWU0.jpg')] bg-repeat-x">
+            <div className="flex justify-center items-center gap-4 py-4">
+                <img className="w-[45px] md:w-[75px]" src="https://i.imgur.com/YeGYiVO.png" alt="" />
+                <h1 className="text-2xl md:text-6xl text-white font-rancho">Espresso Emporium</h1>
+            </div>
+        </nav>
     );
 };
 
