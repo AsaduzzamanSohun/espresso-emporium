3af4bc0b6846cc8c3725beaa7e8b99814b00d858
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-04-23 13:01:26 +0600

    espresso emporium client-side

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <main>
+            <Navbar></Navbar>
+            <h1>Buy me a coffee</h1>
+            <Footer></Footer>
+        </main>
+    );
+};
+
+export default Home;
