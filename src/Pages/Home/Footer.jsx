fea266b35d980c7c2076ce7ded7f9ccbe0f8bf5a
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-04-24 02:29:24 +0600

    footer section done

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -1,31 +1,69 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaPhone, FaTwitter } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-black text-base-content">
-            <aside>
-                <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
-            </aside>
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
+        <footer>
+            <div className=" h-[776px] bg-[url('https://i.imgur.com/4MgIrCf.jpg')]">
+                <div className="max-w-[1440px] md:p-10 mx-auto flex flex-col-reverse md:flex-row justify-between items-center">
+                    <nav className="mt-20 text-center md:text-start">
+
+                        <div className="space-y-2 md:space-y-8 md:mt-8">
+                            <img className="w-[45px] md:w-[75px] mx-auto md:mx-0" src="https://i.imgur.com/YeGYiVO.png" alt="" />
+
+                            <h2 className="text-xl md:text-[45px] font-rancho dark drop-shadow-md">Espresso Emporium</h2>
+
+                            <p className="font-raleway dark text-xs md:text-lg md:leading-loose px-8 md:px-0 max-w-[663px]">
+                                Always ready to be your friend. Come & Contact with us to share your memorable moments, to share with your best companion.
+                            </p>
+                        </div>
+
+                        <div className="flex gap-4 text-[16px] md:text-[40px] dark mt-4 md:mt-8 justify-center md:justify-start">
+                            <span><FaFacebook></FaFacebook></span>
+                            <span><FaTwitter></FaTwitter></span>
+                            <span><FaInstagram></FaInstagram></span>
+                            <span><FaLinkedin></FaLinkedin></span>
+                        </div>
+
+                        <h2 className="md:text-[45px] font-rancho dark drop-shadow-md mt-6">Get in Touch</h2>
+
+                        <div className="md:space-y-4 mt-4 md:mt-8 text-center mx-auto flex flex-col items-center md:items-start">
+                            <div className="flex gap-4 md:text-[20px] dark text-xs">
+                                <span><FaPhone></FaPhone></span>
+                                <p className="font-raleway dark">
+                                    +88 01533 333 333
+                                </p>
+                            </div>
+                            <div className="flex gap-4 md:text-[20px] dark text-xs">
+                                <span><MdMail></MdMail></span>
+                                <p className="font-raleway dark">info@gmail.com</p>
+                            </div>
+                            <div className="flex gap-4 md:text-[20px] dark text-xs">
+                                <span><FaLocationArrow></FaLocationArrow></span>
+                                <p className="font-raleway dark">72, Wall street, King Road, Dhaka</p>
+                            </div>
+                        </div>
+                    </nav>
+
+                    <nav className="mt-12 md:mt-36">
+                        <h2 className="md:text-[45px] font-rancho dark drop-shadow-md mb-4 text-center md:text-start">Get in Touch</h2>
+                        <form className="flex flex-col gap-4 max-w-[503px] md:w-[503px]" action="">
+                            <input className="w-full px-4 py-2 font-raleway" type="text" name="name" placeholder="Name" />
+                            <input className="w-full px-4 py-2 font-raleway" type="email" name="email" placeholder="Email" />
+                            <textarea className="w-full px-4 py-2 font-raleway resize-none" name="message" id="" cols="30" rows="4" placeholder="Message"></textarea>
+                            <input className="submit text-sm md:text-lg max-w-[150px] mx-auto md:mx-0" type="submit" value="Send Message" />
+                        </form>
+                    </nav>
+                </div>
+            </div>
+
+
+
+            <div className="bg-[url('https://i.imgur.com/Rx8dmxo.jpg')] h-[50px] flex justify-center items-center md:text-lg">
+                <p className="font-rancho text-white text-center">Copyright Espresso Emporium ! All Rights Reserved</p>
+            </div>
+
+
         </footer>
     );
 };
